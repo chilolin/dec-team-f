633b9998da3b68b0a4a78df5cc626fc52c21d494
chilolin <yuma321ball@icloud.com>
2022-01-21 16:03:24 +0900

    [add] mattersテーブルのマイグレーション

---
+++ database/migrations/2022_01_21_154833_create_matters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMattersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->foreignId('client_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matters');
+    }
+}
