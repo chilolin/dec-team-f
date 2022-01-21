85589402366503e33532ce09269fc732d38f8ab3
chilolin <yuma321ball@icloud.com>
2022-01-21 16:02:45 +0900

    [update] マスターフラグのデフォルト値を設定

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_master')->default(false);
             $table->timestamps();
-            $table->boolean('is_master');
         });
     }
 
