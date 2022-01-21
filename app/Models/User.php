a2b19c832d508a7e8cd357c3100695023c99c4cf
chilolin <yuma321ball@icloud.com>
2022-01-21 17:20:10 +0900

    [add] skill_userテーブルの作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * このユーザーに属するスキル
+     */
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class)->withTimestamps();
+    }
 }
