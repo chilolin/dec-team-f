f969884a741927ef172db294b4eb5a995d68f41d
chilolin <yuma321ball@icloud.com>
2022-01-21 16:13:53 +0900

    [add] skillsテーブルのマイグレーションを設定

---
+++ database/migrations/2022_01_21_154921_create_skills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->enum('skill_type', ['language', 'framework', 'design_pattern', 'process', 'proceeding', 'engineer_type', 'position', 'database', 'infrastructure']);
+            $table->string('name', 100);
+            $table->float('version')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+}
