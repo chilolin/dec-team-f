9a843d3afa152a81bbeec136120a86567984821a
chilolin <yuma321ball@icloud.com>
2022-01-30 17:04:30 +0900

    [update] ファイル構成を修正

--- resources/views/components/employee/delete-skill-input-button.blade.php
+++ resources/views/components/employee/delete-skill-input-button.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
     <div class="col-md-10">
-        <button class="btn btn-danger btn-sm" onclick="event.preventDefault(); this.closest('#skill-input-group').remove();">削除する</button>
+        <button type="button" class="btn btn-danger btn-sm" onclick="event.preventDefault(); this.closest('#skill-input-group').remove();">削除する</button>
     </div>
 </div>
