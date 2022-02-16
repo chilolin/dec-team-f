68aa970d1ef88aa6a2e11d9f4d20a86c67e9af3e
chilolin <yuma321ball@icloud.com>
2022-02-16 10:26:36 +0900

    [add] モーダルで案件を登録できるようにした

---
+++ app/Http/Controllers/ModalController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class ModalController extends Controller
+{
+    public function store(Request $request)
+    {
+        User::find(Auth::id())
+        ->skills()
+        ->attach(
+            collect($request->skills)
+            ->reduce(function($attach_skills, $specific_skills) {
+                foreach($specific_skills as $skill) {
+                    if (!array_key_exists('checked', $skill)) continue;
+                    $attach_skills[$skill['id']] = ['level' => $skill['level'] ? $skill['level'] : 0.50, 'is_practice' => true];
+                }
+
+                return $attach_skills;
+            }, [])
+        );
+
+        return redirect()->route('employees.show', ['id' => Auth::id()]);
+    }
+}
