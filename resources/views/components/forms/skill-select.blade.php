f095de7337dcece4df05f0c5204a36f2c9278ad5
chilolin <yuma321ball@icloud.com>
2022-02-17 18:08:47 +0900

    [add] スキル選択のインプットを作成

---
+++ resources/views/components/forms/skill-select.blade.php
@@ -0,0 +1,30 @@
+<div class="skill-select-wrapper">
+    <input
+        type="text"
+        class="form-control"
+        data-role="tagsinput"
+    />
+
+    <script>
+        (function($) {
+            $(function() {
+                const candidates = new Bloodhound({
+                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('text'),
+                    queryTokenizer: Bloodhound.tokenizers.whitespace,
+                    local: @json($candidates),
+                });
+                candidates.initialize();
+
+                $('input[data-role="tagsinput"]').tagsinput({
+                    itemValue: 'value',
+                    itemText: 'text',
+                    typeaheadjs: {
+                        name: 'candidates',
+                        displayKey: 'text',
+                        source: candidates.ttAdapter()
+                    }
+                });
+            });
+        })(window.jQuery);
+    </script>
+</div>
