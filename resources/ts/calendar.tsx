c6b5ab93754c506226530140f8a7acb5e142a442
chilolin <yuma321ball@icloud.com>
2022-02-23 15:17:58 +0900

    [update] 案件情報をバックエンドから取得してくる

--- resources/ts/calendar.tsx
+++ resources/ts/calendar.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import FullCalendar from "@fullcalendar/react";
+import axios from "axios";
+import FullCalendar, { EventApi, EventInput } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
 
+type Matter = {
+    id: number;
+    name: string;
+    client_id: number;
+    start_at: string;
+    end_at: string;
+    created_at: Date;
+    updated_at: Date;
+};
+
+const createApiUrl = (pathname: string) => {
+    return `${location.protocol}//${location.host}/api/${pathname}`;
+};
+
 const Calendar = () => {
+    const [initialEvents, setInitialEvents] = useState<EventInput[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(createApiUrl("calendar"));
+            const data: Matter[] = response.data;
+            const eventInput: EventInput[] = data.map((matter) => ({
+                id: matter.id.toString(),
+                title: matter.name,
+                start: new Date(matter.start_at),
+                // end: matter.end_at,
+            }));
+            setInitialEvents(eventInput);
+        })();
+    }, []);
+
     return (
         <div>
             <FullCalendar
                 plugins={[dayGridPlugin]}
                 initialView="dayGridMonth"
+                initialEvents={initialEvents}
                 locales={allLocales}
                 locale="ja"
             />
